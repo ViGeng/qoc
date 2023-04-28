a4e12fada19d46252d065d35fa7ead9ad255a40e
Rowan GENG <rowan.gw@outlook.com>
2023-04-28 17:48:27 +0800

    implement python script to implement: 1. NamedData class 2. test both transparent and opaque functions.

--- NamedData.py
+++ NamedData.py
@@ -1,14 +1,34 @@
 class NamedData:
-    def __init__(self, func, data):
+    '''
+    _FUNC, _DATA represent recursive boundrary conditions
+    '''
+    def __init__(self, name:str, func, data, result=None):
+
+        self.name = name
+
+        if func not in ['_FUNC', '_DATA']:
+            assert type(func) == NamedData
+            assert type(data) == NamedData
+
         self.func = func
         self.data = data
-        self.result = None
+        self.result = result
 
-    def my_function(self):
-        # function code here
-        pass
+    def check_params(self, para):
+        print(para)
+        if para in ['_FUNC', '_DATA']:
+            return True
+        if type(para) == NamedData:
+            return True
+        return False
 
     def reduce(self):
-        if self.result is None:
-            self.result = self.func(self.data)
-        return self.result
+        if self.result is not None:
+            return self.result
+        if self.func in ['_FUNC', '_DATA']:
+            self.result = self.data
+            return self.data
+        # reduce
+        self.reduced_func = self.func.reduce()
+        self.reduced_data = self.data.reduce()
+        return self.reduced_func(self.reduced_data)
--- main.py
+++ main.py
@@ -1,12 +1,77 @@
+import random
 from NamedData import NamedData
 
+FUNC = '_FUNC'
+DATA = '_DATA'
 
-def test_reduce():
-    func = lambda x: x + 1
-    nd = NamedData(func, 1)
-    assert nd.reduce() == 2
 
+def get_nd_func(type: str) -> NamedData:
+    if type == 'func/list_sum':
+        def add_list(num_list: list) -> int:
+            sum = 0
+            for num in num_list:
+                sum += num
+            return sum
+
+        return NamedData(
+            'func/list_sum',
+            FUNC,
+            add_list,
+        )
+
+
+def get_nd_data(type_str: str):
+    if type_str == 'data/123':
+        list = [1, 2, 3]
+        return NamedData(
+            'data/123',
+            DATA,
+            list,
+        ), sum(list)
+    if type_str == 'data/2random':
+        # Generate two random numbers
+        num1 = random.randint(0, 10)
+        num2 = random.randint(0, 10)
+        # Create a list with the two numbers
+        numbers = [num1, num2]
+        return NamedData(
+            'data/2random',
+            DATA,
+            numbers,
+        ), sum(numbers)
+
+def get_opaque_nd_case():
+    data, expected = get_nd_data('data/2random')
+    add2random = NamedData(
+        'data/add2random',
+        get_nd_func('func/list_sum'),
+        data
+    )
+    return add2random, expected
+
+
+def get_transparent_nd_case():
+
+    data, expected = get_nd_data('data/123')
+    add123 = NamedData(
+        'data/add123',
+        get_nd_func('func/list_sum'),
+        data
+    )
+    return add123, expected
+
+
+def test_func():
+    nd_list_sum, expected_value = get_transparent_nd_case()
+    assert nd_list_sum.reduce() == expected_value
+    print('Test test_transparent_func passed!')
+
+    add2random, expected_value = get_opaque_nd_case()
+    assert add2random.reduce() == expected_value
+    print('Test test_opaque_func passed!')
+
+def main():
+    test_func()
 
 if __name__ == '__main__':
-    test_reduce()
-    print('Test passed')
+    main()
